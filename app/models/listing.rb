b9b823190ac7614a3295ef7ccf5461612c23e9d1
Wes Reed <wesley.a.reed@gmail.com>
2016-05-30 14:08:04 -0400

    Update form with file upload field. Add paperclip gem validation. Add image to listing show page

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,5 @@
 class Listing < ActiveRecord::Base
   has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.jpg"
+
+  validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
