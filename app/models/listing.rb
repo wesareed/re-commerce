c5ca8f54d6cad98f8b58d293985fa622a4984836
Wes Reed <wesley.a.reed@gmail.com>
2016-05-31 09:16:43 -0400

    Remove paper-clip dropbox gem

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,7 +1,5 @@
 class Listing < ActiveRecord::Base
-  has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.jpg",
-                    :storage => :dropbox,
-                    :dropbox_credentials => Rails.root.join("config/dropbox.yml"),
+  has_attached_file :image, styles: { medium: "200x", thumb: "100x100>" }, default_url: "default.jpg"
 
   validates_attachment :image, content_type: { content_type: ["image/jpg", "image/jpeg", "image/png", "image/gif"] }
 end
