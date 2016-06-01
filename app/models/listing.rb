be1bd807fa583211d4cfa318fcd2751b180e2deb
Wes Reed <wesley.a.reed@gmail.com>
2016-06-01 13:44:39 -0400

    Add bootstrap styling to listings error message

--- app/models/listing.rb
+++ app/models/listing.rb
@@ -1,3 +1,6 @@
 class Listing < ActiveRecord::Base
   mount_uploader :image, ImageUploader
+
+  validates :name, :description, :price, presence: true
+  validates :price, numericality: { greater_than: 0 }
 end
