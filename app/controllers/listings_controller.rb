c1410ba7d5998cba4605a7f510e47f205098a8c2
Wes Reed <wesley.a.reed@gmail.com>
2016-06-01 16:44:05 -0400

    Create seller listings view route and add seller method to listings controller

--- app/controllers/listings_controller.rb
+++ app/controllers/listings_controller.rb
@@ -1,7 +1,11 @@
 class ListingsController < ApplicationController
   before_action :set_listing, only: [:show, :edit, :update, :destroy]
-  before_action :authenticate_user!, only: [:new, :create, :edit, :update, :destroy]
+  before_action :authenticate_user!, only: [:seller, :new, :create, :edit, :update, :destroy]
   before_action :check_user, only: [:edit, :update, :destroy]
+
+  def seller
+    @listings = Listing.where(user: current_user)
+  end
 
   # GET /listings
   # GET /listings.json
