a535cfbac2e10f582db03f13203f5cab743479e4
Wes Reed <wesley.a.reed@gmail.com>
2016-06-01 11:57:58 -0400

    Remove paperclip migration

--- db/schema.rb
+++ db/schema.rb
@@ -20,12 +20,8 @@ ActiveRecord::Schema.define(version: 20160531205654) do
     t.string   "name"
     t.text     "description"
     t.decimal  "price"
-    t.datetime "created_at",         null: false
-    t.datetime "updated_at",         null: false
-    t.string   "image_file_name"
-    t.string   "image_content_type"
-    t.integer  "image_file_size"
-    t.datetime "image_updated_at"
+    t.datetime "created_at",  null: false
+    t.datetime "updated_at",  null: false
     t.string   "image"
   end
 
